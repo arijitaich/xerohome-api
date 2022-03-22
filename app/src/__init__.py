5402b4e77d9d6af76531194997d0bbf154d3b781
Arijit Aich <a2.arijitaich@gmail.com>
2022-03-22 18:08:16 +0530

    Core Update

---
+++ app/src/__init__.py
@@ -0,0 +1,29 @@
+
+
+# from curses import flash
+from flask import Flask, jsonify
+
+def create_app(test_config=None):
+    
+    
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY = "dev"
+
+        )
+
+    else:
+        app.config.from_mapping(test_config)
+
+
+    @app.route("/")
+    def index():
+        return "Connected: {testing}"
+
+    @app.route("/status")
+    def status():
+        return jsonify({"status": "Connected"})
+
+    return app
